ef7f929900cd349b799945f60cf8e284bb558af6
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-30 02:05:36 +0600

    fiirebase utili line removed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { async } = require("@firebase/util");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pha46.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
